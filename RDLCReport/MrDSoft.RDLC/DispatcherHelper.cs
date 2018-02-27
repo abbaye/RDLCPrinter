c6e2e6d95f1be78ba15825227af80cbed8836b90
Derek Tremblay <khenshin@msn.com>
2018-02-26 22:05:58 -0500

    Modified some code and added nuget sqlserverCe unofficial for sample

--- RDLCReport/MrDSoft.RDLC/DispatcherHelper.cs
+++ RDLCReport/MrDSoft.RDLC/DispatcherHelper.cs
@@ -6,39 +6,45 @@ namespace DSoft.RDLCReport
     /// <summary>
     /// WPF UI Dispatcher
     /// <remarks>
-    /// CREDIT : Alex Petrescu 2010, Modified 2014 by Derek Tremblay
+    /// CREDIT : Alex Petrescu 2010, Modified 2014 and 2018 by Derek Tremblay
     /// PROFIL : http://social.msdn.microsoft.com/profile/alex%20petrescu/?ws=usercard-inline
     /// http://social.msdn.microsoft.com/Forums/fr-FR/248a3258-e3ec-4ba2-9085-2fda2f0b0058/wpf-faq-applicationdoevents-dans-wpf?forum=wpffr
     /// </remarks>
     /// </summary>
-    public class DispatcherHelper
+    public static class DispatcherHelper
     {
-        private static DispatcherOperationCallback exitFrameCallback = new DispatcherOperationCallback(ExitFrame);
-        
+        private static readonly DispatcherOperationCallback ExitFrameCallback = ExitFrame;
+
         /// <summary>
         /// Execute all message in message Queud
         /// </summary>
-        public static void DoEvents()
-        {            
+        public static void DoEvents(DispatcherPriority priority = DispatcherPriority.Background)
+        {
             var nestedFrame = new DispatcherFrame();
-            
-            var exitOperation = Dispatcher.CurrentDispatcher.BeginInvoke( DispatcherPriority.Background, exitFrameCallback, nestedFrame);
+            var exitOperation = Dispatcher.CurrentDispatcher.BeginInvoke(priority, ExitFrameCallback, nestedFrame);
 
-            //execute all next message
-            Dispatcher.PushFrame(nestedFrame);
+            try
+            {
+                //execute all next message
+                Dispatcher.PushFrame(nestedFrame);
 
-            //If not completed, will stop it
-            if (exitOperation.Status != DispatcherOperationStatus.Completed)
+                //If not completed, will stop it
+                if (exitOperation.Status != DispatcherOperationStatus.Completed)
+                    exitOperation.Abort();
+            }
+            catch
+            {
                 exitOperation.Abort();
-            
+            }
+
         }
 
         private static Object ExitFrame(Object state)
         {
-            var frame = state as DispatcherFrame;
-
             // exit the message loop
-            frame.Continue = false;
+            if (state is DispatcherFrame frame)
+                frame.Continue = false;
+
             return null;
         }
     }
---
+++ RDLCReport/MrDSoft.RDLC/LightIntegerSpinner.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Windows;
+using DSoft.MethodExtension;
+
+namespace DSoft.RDLCReport
+{
+    /// <summary>
+    /// LightIntegerSpinner
+    /// <remarks>
+    /// CREDIT : 2013-2014 Derek Tremblay (abbaye)
+    /// https://github.com/abbaye/RDLCPrinter
+    /// </remarks>
+    /// </summary>
+    public partial class LightIntegerSpinner
+    {
+        private int _minimum;
+        private int _maximum = 50;
+        private bool _isShowSpinnerButton = true;
+
+        public event EventHandler ValueChanged;
+        public event EventHandler UpButtonClick;
+        public event EventHandler DownButtonClick;
+        private bool _IsShowCurrentToMaximumValue;
+
+
+        public LightIntegerSpinner()
+        {
+            InitializeComponent();
+        }
+
+        public int? Value
+        {
+            get
+            {
+                try
+                {
+                    if (_IsShowCurrentToMaximumValue)                    
+                        return Convert.ToInt32(NumPager.Text.Split('/')[0].Trim());                    
+                    else                    
+                        return Convert.ToInt32(NumPager.Text);                    
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                if (_IsShowCurrentToMaximumValue)
+                {
+                    NumPager.Text = value + " / " + _maximum;
+                }else
+                    NumPager.Text = value.ToString();
+
+                CheckRange();
+                UpdateButton();
+
+                if (ValueChanged != null)
+                    ValueChanged(this, new EventArgs());
+            }
+        }
+
+        public bool IsShowSpinnerButton
+        {
+            get
+            {
+                return _isShowSpinnerButton;
+            }
+            set
+            {
+                _isShowSpinnerButton = value;
+
+                if (value)
+                    ButtonColumn.Width = new GridLength(20);
+                else
+                    ButtonColumn.Width = new GridLength(0);
+            }
+        }
+
+        private void UpdateButton()
+        {
+            SpinnerUp.DisableButton();
+            SpinnerDown.DisableButton();
+
+            if (Value > _minimum)
+                SpinnerDown.EnableButton();
+
+            if (Value < _maximum)
+                SpinnerUp.EnableButton();
+        }
+
+        public int Minimum
+        {
+            get
+            {
+                return _minimum;
+            }
+            set
+            {
+                _minimum = value;
+
+                CheckRange();
+                UpdateButton();
+            }
+        }
+
+        public int Maximum
+        {
+            get
+            {
+                return _maximum;
+            }
+            set
+            {
+                _maximum = value;
+
+                CheckRange();
+                UpdateButton();
+            }
+        }
+
+        public bool IsShowCurrentToMaximumValue
+        {
+            get
+            {
+                return _IsShowCurrentToMaximumValue;
+            }
+            set
+            {
+                _IsShowCurrentToMaximumValue = value;
+
+                Value = Value;
+            }
+        }
+        
+
+        private void CheckRange()
+        { 
+
+            if (Value > _maximum)
+            {
+                Value = _maximum;
+
+                if (ValueChanged != null)
+                    ValueChanged(this, new EventArgs());
+            }
+
+            if (Value < _minimum)
+            {
+                Value = _minimum;
+
+                if (ValueChanged != null)
+                    ValueChanged(this, new EventArgs());
+            }
+        }
+
+        private void SpinnerUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (Value < _maximum)
+                Value += 1;
+
+            CheckRange();
+            UpdateButton();
+
+            if (UpButtonClick != null)
+                UpButtonClick(this, new EventArgs());
+        }
+
+        private void SpinnerDown_Click(object sender, RoutedEventArgs e)
+        {
+            if (Value > _minimum)
+                Value -= 1;
+
+            CheckRange();
+            UpdateButton();
+
+            if (DownButtonClick != null)
+                DownButtonClick(this, new EventArgs());
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            CheckRange();
+            UpdateButton();
+        }
+    }
+}
